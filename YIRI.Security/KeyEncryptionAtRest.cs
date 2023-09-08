39e55ced5c80fbf3cc152a1444f65c9ff30c6399
yanguoqiang <yanguoqiang@yiritech.com>
2023-09-08 15:58:27 +0800

    添加项目文件。

---
+++ YIRI.Security/KeyEncryptionAtRest.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.DataProtection.XmlEncryption;
+using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace YIRI.Security
+{
+    internal static class AesKeyingMaterial
+    {
+        internal static readonly byte[] Key = Convert.FromBase64String("68jgB2lhZ/UYr14aSzi4AKsaeiS2jQvcYIJDhhzwcws=");
+        internal static readonly byte[] IV = Convert.FromBase64String("6VYIsDg5VDfRWR0KYCp5Dw==");
+        internal const string KEY_NAME = "YIRI_AES";
+    }
+
+    public sealed class AesXmlEncryptor : IXmlEncryptor
+    {
+        public EncryptedXmlInfo Encrypt(XElement plaintextElement)
+        {
+            if (plaintextElement is null) throw new ArgumentNullException(nameof(plaintextElement));
+
+            // <EncryptedData Type="http://www.w3.org/2001/04/xmlenc#Element" xmlns="http://www.w3.org/2001/04/xmlenc#">
+            //   ...
+            // </EncryptedData>
+
+            XElement encryptedElement;
+            // Create an AesCryptoServiceProvider object
+            // with the specified key and IV.
+            using (Aes yiriAes = Aes.Create())
+            {
+                yiriAes.Key = AesKeyingMaterial.Key;
+                yiriAes.IV = AesKeyingMaterial.IV;
+                encryptedElement = EncryptElement(plaintextElement, yiriAes);
+            }
+            return new EncryptedXmlInfo(encryptedElement, typeof(AesXmlDecryptor));
+        }
+
+        private XElement EncryptElement(XElement plaintextElement, SymmetricAlgorithm keyObj)
+        {
+            if (keyObj is null) throw new ArgumentNullException(nameof(keyObj));
+            // EncryptedXml works with XmlDocument, not XLinq. When we perform the conversion
+            // we'll wrap the incoming element in a dummy <root /> element since encrypted XML
+            // doesn't handle encrypting the root element all that well.
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(new XElement("root", plaintextElement).CreateReader());
+            var elementToEncrypt = (XmlElement)xmlDocument.DocumentElement!.FirstChild!;
+
+            // Perform the encryption and update the document in-place.
+            var encryptedXml = new EncryptedXml(xmlDocument);
+            encryptedXml.AddKeyNameMapping(AesKeyingMaterial.KEY_NAME, keyObj);
+            var encryptedData = encryptedXml.Encrypt(elementToEncrypt, AesKeyingMaterial.KEY_NAME);
+            EncryptedXml.ReplaceElement(elementToEncrypt, encryptedData, content: false);
+
+            // Strip the <root /> element back off and convert the XmlDocument to an XElement.
+            return XElement.Load(xmlDocument.DocumentElement.FirstChild!.CreateNavigator()!.ReadSubtree());
+        }
+
+    }
+
+    internal sealed class AesXmlDecryptor : IXmlDecryptor
+    {
+        /// <summary>
+        /// Can decrypt the XML key data from an <see cref="Aes"/>.
+        /// </summary>
+        private class EncryptedXmlWithAesKeys : EncryptedXml
+        {
+            private readonly SymmetricAlgorithm? _keyObj;
+            public EncryptedXmlWithAesKeys(XmlDocument document, SymmetricAlgorithm? keyObj)
+                : base(document)
+            {
+                _keyObj = keyObj;
+            }
+
+            public override byte[] DecryptEncryptedKey(EncryptedKey encryptedKey)
+            {
+                if (_keyObj != null) return DecryptKey(encryptedKey.CipherData.CipherValue, _keyObj);
+                return base.DecryptEncryptedKey(encryptedKey);
+            }
+
+        }
+
+        public XElement Decrypt(XElement encryptedElement)
+        {
+            if (encryptedElement is null) throw new ArgumentNullException(nameof(encryptedElement));
+
+            XmlDocument xmlDocument;
+            using (Aes yiriAes = Aes.Create())
+            {
+                yiriAes.Key = AesKeyingMaterial.Key;
+                yiriAes.IV = AesKeyingMaterial.IV;
+                // <EncryptedData Type="http://www.w3.org/2001/04/xmlenc#Element" xmlns="http://www.w3.org/2001/04/xmlenc#">
+                //   ...
+                // </EncryptedData>
+
+                // EncryptedXml works with XmlDocument, not XLinq. When we perform the conversion
+                // we'll wrap the incoming element in a dummy <root /> element since encrypted XML
+                // doesn't handle encrypting the root element all that well.
+                xmlDocument = new XmlDocument();
+                xmlDocument.Load(new XElement("root", encryptedElement).CreateReader());
+
+                // Perform the decryption and update the document in-place.
+                var encryptedXml = new EncryptedXmlWithAesKeys(xmlDocument, yiriAes);
+
+                encryptedXml.DecryptDocument();
+            }
+
+            // Strip the <root /> element back off and convert the XmlDocument to an XElement.
+            return XElement.Load(xmlDocument.DocumentElement!.FirstChild!.CreateNavigator()!.ReadSubtree());
+        }
+    }
+
+}
---
+++ dataProtector/Class1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+using YIRI.Security;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string appname = "YIRITECH_APP";
+        Console.WriteLine(Path.Combine(Environment.CurrentDirectory, "DataProtection-Keys"));
+        // add data protection services
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.Configure<KeyManagementOptions>(opt => opt.XmlEncryptor = new AesXmlEncryptor());
+        serviceCollection.AddDataProtection()
+            .PersistKeysToFileSystem(new DirectoryInfo(Path.Combine(Environment.CurrentDirectory, "DataProtection-Keys")))
+            .SetApplicationName(appname)
+            .SetDefaultKeyLifetime(TimeSpan.FromDays(365));
+
+        var services = serviceCollection.BuildServiceProvider();
+
+        // create an instance of MyClass using the service provider
+        var instance = ActivatorUtilities.CreateInstance<MyClass>(services);
+        instance.RunSample();
+    }
+
+    public class MyClass
+    {
+        IDataProtector _protector;
+        string purpose = "YIRI.Security.Purpose.SensitiveDataPurpose"; 
+
+        // the 'provider' parameter is provided by DI
+        public MyClass(IDataProtectionProvider provider)
+        {
+            _protector = provider.CreateProtector(purpose);
+
+        }
+
+        public void RunSample()
+        {
+            try
+            {
+                string input = "6a7be2fee9bb-2023/09/08 15:07:41-2024/09/08 23:59:59";
+                string sec = "CfDJ8Jevqcuq1MxNsuygwP_cBmy-OWX0odZblhpIIVTrE_vkZAW3Scc1jyFSzrBQhFVuPwZ37IOQd02WqPKIDNE64MeW_0FHx_8YfgMdqnfow7cyn_p4-N-8Vq10tvmicmdEIT2HG-1NKMNurgkIFXuc--9OGBVWKKJ48nVmSAXcf1aM_QJNJtihBebETYTVFyB75g";
+                Console.WriteLine($"Enter input: {input}");
+
+                // protect the payload
+                string protectedPayload = _protector.Protect(input);
+                Console.WriteLine($"Protect returned: {protectedPayload}");
+
+                // unprotect the payload
+                string unprotectedPayload = _protector.Unprotect(protectedPayload);
+                Console.WriteLine($"Unprotect returned: {unprotectedPayload}");
+                Debug.Assert(input == unprotectedPayload);
+
+                //加密结果会不一样，但是仍可以解密的
+                Console.WriteLine("解密：");
+                Console.WriteLine(_protector.Unprotect(sec));
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+    }
+}
+
+/*
+ * SAMPLE OUTPUT
+ *
+ * Enter input: Hello world!
+ * Protect returned: CfDJ8ICcgQwZZhlAlTZT...OdfH66i1PnGmpCR5e441xQ
+ * Unprotect returned: Hello world!
+ */
